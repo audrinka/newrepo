bc681eb245e0bf391e8d24227111bc240be46045
Agnieszka <agnieszkaklosik@hotmail.com>
2016-09-04 00:00:53 +0200

    works, additional task not included

---
+++ App.js
@@ -0,0 +1,43 @@
+// OGÓLNA FUNKCJA
+function randomString() {
+	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+	var str = '', i;
+	for (i = 0; i < 10; i++) {
+	  str += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return str;
+}
+
+// KOMUNIKACJA Z SERWEREM
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '35',
+  'X-Auth-Token': 'b59294ef16812d79d45250e43746b568'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+ 		var col = new Column(column.id, column.name);
+    	board.createColumn(col);
+    	setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
